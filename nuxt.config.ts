0e2ef2560392e11475c699b695afe21d6062709f
Pascal Sthamer <10992664+P4sca1@users.noreply.github.com>
2024-09-22 13:57:16 +0200

    Add nuxt-security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,64 +1,65 @@
 export default defineNuxtConfig({
-	telemetry: false,
+				telemetry: false,
 
-	compatibilityDate: '2024-09-10',
+				compatibilityDate: '2024-09-10',
 
-	future: {
-		compatibilityVersion: 4,
-	},
-
-	typescript: {
-		shim: false,
-	},
-
-	modules: [
-		'@nuxt/eslint',
-		'@nuxt/content',
-		'@nuxtjs/tailwindcss',
-		'@nuxtjs/color-mode',
-		'@nuxthq/studio',
-		'@vueuse/nuxt',
-		'@nuxt/image',
-		'nuxt-headlessui',
-	],
-
-	app: {
-		head: {
-			title: 'Studentischer Wahlausschuss - TH Köln',
-			viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
-			htmlAttrs: {
-				lang: 'de',
-			},
-			meta: [
-				{ property: 'og:type', content: 'website' },
-				{
-					property: 'og:site_name',
-					content: 'Studentischer Wahlausschuss - TH Köln',
+				future: {
+								compatibilityVersion: 4,
 				},
-				{ property: 'og:locale', content: 'de_DE' },
-			],
-			link: [],
-		},
-	},
 
-	content: {
-		// https://content.nuxtjs.org/api/configuration#documentdriven-options
-		documentDriven: {
-			navigation: false,
-			page: true,
-			surround: false,
-			globals: {},
-			layoutFallbacks: [],
-			injectPage: true,
-		},
-	},
+				typescript: {
+								shim: false,
+				},
 
-	colorMode: {
-		classSuffix: '',
-	},
+				modules: [
+				 '@nuxt/eslint',
+				 '@nuxt/content',
+				 '@nuxtjs/tailwindcss',
+				 '@nuxtjs/color-mode',
+				 '@nuxthq/studio',
+				 '@vueuse/nuxt',
+				 '@nuxt/image',
+				 'nuxt-headlessui',
+				 'nuxt-security',
+				],
 
-	image: {
-		provider: 'ipx',
-		format: ['webp'],
-	},
+				app: {
+								head: {
+												title: 'Studentischer Wahlausschuss - TH Köln',
+												viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
+												htmlAttrs: {
+																lang: 'de',
+												},
+												meta: [
+																{ property: 'og:type', content: 'website' },
+																{
+																				property: 'og:site_name',
+																				content: 'Studentischer Wahlausschuss - TH Köln',
+																},
+																{ property: 'og:locale', content: 'de_DE' },
+												],
+												link: [],
+								},
+				},
+
+				content: {
+								// https://content.nuxtjs.org/api/configuration#documentdriven-options
+								documentDriven: {
+												navigation: false,
+												page: true,
+												surround: false,
+												globals: {},
+												layoutFallbacks: [],
+												injectPage: true,
+								},
+				},
+
+				colorMode: {
+								classSuffix: '',
+				},
+
+				image: {
+								provider: 'ipx',
+								format: ['webp'],
+				},
 })
